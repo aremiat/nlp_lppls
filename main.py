fff192a183e575249d2d5071f95a2faa78091e20
Remiat Alexandre <134190489+aremiat@users.noreply.github.com>
2025-07-24 19:30:47 +0200

    deleted nlcnn

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from GQLib.Optimizers.NELDER_MEAD import NELDER_MEAD
 from GQLib.Optimizers.Neural_Network.MLNN import MLNN
 from GQLib.Optimizers.Neural_Network.RNN import RNN
 from GQLib.Optimizers.Neural_Network.CNN import CNN
-from GQLib.Optimizers.Neural_Network.NLCNN import NLCNN
-from GQLib.Optimizers.Neural_Network.attention import NLCNNWithAttention, MLNNWithAttention, CNNWithAttention, RNNWithAttention
+from GQLib.Optimizers.Neural_Network.attention import MLNNWithAttention, CNNWithAttention, RNNWithAttention
 from GQLib.Models.LPPLS import LPPLS
 from GQLib.subintervals import ClassicSubIntervals
 from GQLib.AssetProcessor import AssetProcessor
@@ -22,10 +21,10 @@ wti.compare_optimizers(frequency="daily",
                                     RNN(LPPLS, epochs=100, silent=True),
                                     CNN(LPPLS, epochs=100, silent=True)],
                        significativity_tc=0.3,
-                       rerun=True,
+                       rerun=False,
                        nb_tc=10,
                        subinterval_method=ClassicSubIntervals,
-                       save=True,
+                       save=False,
                        plot=False)
 
 wti = AssetProcessor(input_type = InputType.BTC)
@@ -36,10 +35,10 @@ wti.compare_optimizers(frequency="daily",
                                     RNN(LPPLS, epochs=100, silent=True),
                                     CNN(LPPLS, epochs=100, silent=True)],
                        significativity_tc=0.3,
-                       rerun=True,
+                       rerun=False,
                        nb_tc=10,
                        subinterval_method=ClassicSubIntervals,
-                       save=True,
+                       save=False,
                        plot=False)
 
 wti = AssetProcessor(input_type = InputType.SP500)
@@ -50,10 +49,10 @@ wti.compare_optimizers(frequency="daily",
                                     RNN(LPPLS, epochs=100, silent=True),
                                     CNN(LPPLS, epochs=100, silent=True)],
                        significativity_tc=0.3,
-                       rerun=True,
+                       rerun=False,
                        nb_tc=10,
                        subinterval_method=ClassicSubIntervals,
-                       save=True,
+                       save=False,
                        plot=False)
 
 # PLotting the results
@@ -161,11 +160,48 @@ plt.show(block=True)
 # # custom_net = CustomNet(n_hidden=64)
 # #
 # # # Use the custom network in MLNN
-# wti.compare_optimizers(frequency="daily",
-#                        optimizers=[RNN(LPPLS, net=custom_net)],
-#                        significativity_tc=0.3,
-#                        rerun=True,
-#                        nb_tc=10,
-#                        subinterval_method=ClassicSubIntervals,
-#                        save=True,
-#                        plot=True)
+
+wti.compare_optimizers(frequency="daily",
+                       optimizers=[NELDER_MEAD(LPPLS),
+                           RNN(LPPLS, net=RNNWithAttention()),
+                                   MLNN(LPPLS, net=MLNNWithAttention()),
+                                   CNN(LPPLS, net=CNNWithAttention())],
+                       significativity_tc=0.3,
+                       rerun=True,
+                       nb_tc=10,
+                       subinterval_method=ClassicSubIntervals,
+                       save=True,
+                       plot=True)
+
+fw = Framework("daily", InputType.WTI)
+#
+plotter = KernelPlotter.from_framework(fw)
+
+fig, axes = plotter._base("01/04/2003", "02/01/2008", "03/07/2008")
+#
+with open("nlp_results/WTI_metrics.json", "r") as f:
+    data = json.load(f)
+
+plotter._add_half_violins(fig, axes, data["Set 1"],
+                        width_scale=0.5,
+                        spacing=0.75,
+                        specific="tc_distrib_non_significant",
+                        hatch_pattern="\\",
+                        color="white",
+                        text=True,)
+plotter._add_half_violins(fig, axes, data["Set 1"],
+                        width_scale=0.5,
+                        spacing=0.75,
+                        specific="tc_distrib_significant",
+                        hatch_pattern="//",
+                        color="lightgrey",)
+plotter._add_half_violins(fig, axes, data["Set 1"],
+                        width_scale=0.5,
+                        spacing=0.75,
+                        specific="tc_distrib_significant_power",
+                        hatch_pattern="x",
+                        color="grey",)
+
+plt.show(block=True)
+
+fw = Framework("daily", InputType.WTI)
